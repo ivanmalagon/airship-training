6c2813a907c4000f1f67bda84e7762bc831067df
Ivan Malagon <hacheka@gmail.com>
2018-09-24 15:27:17 +0200

    Add room type and responsiveness

--- src/main.js
+++ src/main.js
@@ -26,44 +26,86 @@ function handleCategoriesSelected() {
   });
 }
 
-function filterNeighbourhood (neighbourhoods) {
-  if (!viz) {
-    return;
-  }
+let neighbourhoodFilter = '';
+let roomTypeFilter = '';
 
-  let color = `opacity(rgb(0,0,255), 0.25)`;
+function filterNeighbourhood (neighbourhoods) {
   if (neighbourhoods.length > 0) {
     const formattedData = neighbourhoods.map((neighbourhood) => {
       return `'${neighbourhood}'`;
     });
-    const filterExpression = `$neighbourhood_group in [${formattedData.join(',')}]`;
-    color = `opacity(rgb(0,0,255), ${filterExpression} * 0.25)`;
+    neighbourhoodFilter = `$neighbourhood_group in [${formattedData.join(',')}]`;
+  } else {
+    neighbourhoodFilter = '';
   }
-  viz.color.blendTo(color, 2000);
+  applyFilters();
 }
+
 
 function handleLayerSelector () {
   const entireSwitch = document.getElementById('entire-switch');
   const privateSwitch = document.getElementById('private-switch');
   const sharedSwitch = document.getElementById('shared-switch');
 
-  entireSwitch.addEventListener('change', (event) => {
-    const checked = event.detail;
+  function filterByLayer () {
+    const entireChecked = entireSwitch.checked;
+    const privateChecked = privateSwitch.checked;
+    const sharedChecked = sharedSwitch.checked;
+    const entireValue = "'Entire home/apt'";
+    const privateValue = "'Private room'";
+    const sharedValue = "'Shared room'";
+    const filterValues = [];
+
     let color = `opacity(rgb(0,0,255), 0.25)`;
-    if (checked) {
-      const filterExpression = `$room_type == 'Entire home/apt'`;
-      color = `opacity(rgb(0,0,255), ${filterExpression} * 0.25)`;
+    if (entireChecked) {
+      filterValues.push(entireValue);
     }
-    viz.color.blendTo(color);
+    if (privateChecked) {
+      filterValues.push(privateValue);
+    }
+    if (sharedChecked) {
+      filterValues.push(sharedValue);
+    }
+    if (filterValues.length === 3) {
+      roomTypeFilter = '';
+    } else if (filterValues.length > 0) {
+      roomTypeFilter = `$room_type in [${filterValues.join(',')}]`;
+    } else {
+      roomTypeFilter = `$room_type in ['wadus']`;
+    }
+    applyFilters();
+  }
 
-
+  entireSwitch.addEventListener('change', (event) => {
+    filterByLayer();
   });
   privateSwitch.addEventListener('change', (event) => {
-    console.log('multiple', event.detail);
+    filterByLayer();
   });
   sharedSwitch.addEventListener('change', (event) => {
-    console.log('multiple', event.detail);
+    filterByLayer();
   });
 }
 
-window.onload = onLoad;
+function applyFilters () {
+  const both = neighbourhoodFilter && roomTypeFilter;
+  const color = 'rgb(0,0,255)';
+  const opacity = '0.25';
+  let colorExp = `opacity(${color}, ${opacity})`;
+
+  if (!viz) {
+    return;
+  }
+
+  if (both) {
+    colorExp = `opacity(${color}, ${neighbourhoodFilter} and ${roomTypeFilter} * ${opacity})`;
+  } else if (neighbourhoodFilter) {
+    colorExp = `opacity(${color}, ${neighbourhoodFilter} * ${opacity})`;
+  } else if (roomTypeFilter) {
+    colorExp = `opacity(${color}, ${roomTypeFilter} * ${opacity})`;
+  }
+  viz.color.blendTo(colorExp);
+}
+
+const responsiveContent = document.querySelector('as-responsive-content');
+responsiveContent.addEventListener('ready', onLoad);
